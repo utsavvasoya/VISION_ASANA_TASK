0f28791fb6ac863a3cfdef067115e4b708c08b9b
utsav <utsavvasoya89@gmail.com>
2023-02-07 18:25:48 +0530

    implement authorize.net payment gateway

--- 18-01-2023/controller/paymentController.js
+++ 18-01-2023/controller/paymentController.js
@@ -1,6 +1,9 @@
 const paypal = require('paypal-rest-sdk');
 const paymentSchema = require("../models/payment");
 const stripe = require('stripe')('sk_test_Czcmd6nNU3pu0sUjKGT3TYAf')
+const ApiContracts = require("authorizenet").APIContracts;
+const ApiControllers = require("authorizenet").APIControllers;
+const validator = require('validator');
 
 exports.payForPaypal = async (req, res) => {
     const create_payment_json = {
@@ -96,10 +99,128 @@ exports.paymentStripe = async (req, res) => {
                 paymentCurrency: charge.currency
             });
             await newpayment.save();
-            res.send("Success")
+            res.json({ message: "Success" })
         })
         .catch((err) => {
             console.log(err);
             res.send("Invalid Card number")
         });
+}
+
+exports.authorize = async (req, res) => {
+    const { cc, cvv, expire, amount } = req.body;
+    const errors = [];
+    if (!validator.isCreditCard(cc)) {
+        errors.push({
+            param: 'cc',
+            msg: 'Invalid credit card number.'
+        });
+    }
+
+    if (!/^\d{3}$/.test(cvv)) {
+        errors.push({
+            param: 'cvv',
+            msg: 'Invalid CVV code.'
+        });
+    }
+
+    if (!/^\d{4}$/.test(expire)) {
+        errors.push({
+            param: 'expire',
+            msg: 'Invalid expiration date.'
+        });
+    }
+
+    if (!validator.isDecimal(amount)) {
+        errors.push({
+            param: 'amount',
+            msg: 'Invalid amount.'
+        });
+    }
+    if (errors.length > 0) {
+        return res.json({ error: "Enter valid card details." })
+    }
+
+    const merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+    merchantAuthenticationType.setName(process.env.API_LOGIN_ID);
+    merchantAuthenticationType.setTransactionKey(process.env.TRANSACTION_KEY);
+
+    const creditCard = new ApiContracts.CreditCardType();
+    creditCard.setCardNumber(cc);
+    creditCard.setExpirationDate(expire);
+    creditCard.setCardCode(cvv);
+
+    const paymentType = new ApiContracts.PaymentType();
+    paymentType.setCreditCard(creditCard);
+
+    const transactionSetting = new ApiContracts.SettingType();
+    transactionSetting.setSettingName('recurringBilling');
+    transactionSetting.setSettingValue('false');
+
+    const transactionSettingList = [];
+    transactionSettingList.push(transactionSetting);
+
+    const transactionSettings = new ApiContracts.ArrayOfSetting();
+    transactionSettings.setSetting(transactionSettingList);
+
+    const transactionRequestType = new ApiContracts.TransactionRequestType();
+    transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
+    transactionRequestType.setPayment(paymentType);
+    transactionRequestType.setAmount(amount);
+    transactionRequestType.setTransactionSettings(transactionSettings);
+
+    const createRequest = new ApiContracts.CreateTransactionRequest();
+    createRequest.setMerchantAuthentication(merchantAuthenticationType);
+    createRequest.setTransactionRequest(transactionRequestType);
+    console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+    const ctrl = new ApiControllers.CreateTransactionController(createRequest.getJSON());
+    ctrl.execute(async () => {
+        const apiResponse = ctrl.getResponse();
+        const response = new ApiContracts.CreateTransactionResponse(apiResponse);
+        if (response !== null) {
+            if (response.getMessages().getResultCode() === ApiContracts.MessageTypeEnum.OK) {
+                if (response.getTransactionResponse().getMessages() !== null) {
+                    const newpayment = new paymentSchema({
+                        paymentId: response.transactionResponse.transId,
+                        paymentAmount: amount,
+                        paymentMethod: "Authorize.Net",
+                        paymentCurrency: "USD"
+                    });
+                    await newpayment.save();
+                    console.log('Successfully created transaction with transaction ID: ' + response.getTransactionResponse().getTransId());
+                    console.log('Response Code: ' + response.getTransactionResponse().getResponseCode());
+                    console.log('Message Code: ' + response.getTransactionResponse().getMessages().getMessage()[0].getCode());
+                    console.log('Description: ' + response.getTransactionResponse().getMessages().getMessage()[0].getDescription());
+                    res.json({ message: 'Transaction was successful.' });
+                } else {
+                    if (response.getTransactionResponse().getErrors() !== null) {
+                        let code = response.getTransactionResponse().getErrors().getError()[0].getErrorCode();
+                        let text = response.getTransactionResponse().getErrors().getError()[0].getErrorText();
+                        res.json({
+                            error: `${code}: ${text}`
+                        });
+                    } else {
+                        res.json({ error: 'Transaction failed.' });
+                    }
+                }
+            } else {
+                if (response.getTransactionResponse() !== null && response.getTransactionResponse().getErrors() !== null) {
+                    let code = response.getTransactionResponse().getErrors().getError()[0].getErrorCode();
+                    let text = response.getTransactionResponse().getErrors().getError()[0].getErrorText();
+                    res.json({
+                        error: `${code}: ${text}`
+                    });
+                } else {
+                    let code = response.getMessages().getMessage()[0].getCode();
+                    let text = response.getMessages().getMessage()[0].getText();
+                    res.json({
+                        error: `${code}: ${text}`
+                    });
+                }
+            }
+
+        } else {
+            res.json({ error: 'No response.' });
+        }
+    });
 }
--- 18-01-2023/routes/paymentRoute.js
+++ 18-01-2023/routes/paymentRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { payForPaypal, successPaymentPaypal, paymentStripe } = require('../controller/paymentController');
+const { payForPaypal, successPaymentPaypal, paymentStripe, authorize } = require('../controller/paymentController');
 
 router.post('/pay', payForPaypal)
 router.get('/success', successPaymentPaypal)
 router.get('/cancel', (req, res) => res.send('Cancelled'));
 router.post('/paymentStripe', paymentStripe)
+router.post('/authorize', authorize)
 
 module.exports = router;
